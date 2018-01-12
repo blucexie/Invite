b33799ffd35ec3f661983e43c1132f7a930f615f
谢玉琪 <649453836@qq.com>
2018-01-12 18:45:13 +0800

    邀请页面

---
+++ js/enroll.js
@@ -0,0 +1,26 @@
+/**
+ * Created by blucexie on 2018/1/12.
+ */
+$(function () {
+    $('.noCode').click(function () {
+        $('.warn').addClass('animated swing').show();
+        $('.shade').show();
+    });
+    $('.backBtn').click(function () {
+        $('.warn').hide();
+        $('.shade').hide();
+    })
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
